a210b8110506adf9d1fe9fdb3e4e931485636086
Karson Krieg <klkrieg3@gmail.com>
2020-07-26 19:28:35 -0700

    narrowed style further && added the modalswitch to switch between sign-in/sign-up

--- public/js/landing.js
+++ public/js/landing.js
@@ -40,15 +40,23 @@ subscribe.addEventListener("submit", function (event) {
 
 });
 
+/////////DECLARE DOCUMENT SELECTORS////////
+
 const signInBtn = document.getElementById('sign-in-btn');
 const signUpBtn = document.getElementById('sign-up-btn');
 const signUpWrapper = document.getElementById('sign-up__modal-wrapper');
 const signInWrapper = document.getElementById('sign-in__modal-wrapper');
+const toSignIn = document.getElementById('switch-to-sign-in');
+const toSignUp = document.getElementById('switch-to-sign-up');
 
+////////////ADD EVENTLISTENERS FOR MODAL DISPLAY////////////
 
 signInBtn.addEventListener("click", modalDisplay);
 signUpBtn.addEventListener("click", modalDisplay);
-      
+toSignIn.addEventListener('click', modalSwitch);
+toSignUp.addEventListener('click', modalSwitch);
+
+//////////////MODAL FUNCTIONALITY/////////////
 function closeModal(event){
     if(event.key === 'Escape'){
       signUpWrapper.style.display = 'none';
@@ -56,6 +64,16 @@ function closeModal(event){
     }
 };
 
+function modalSwitch({target}){
+  console.log(target.id);
+  if(target.id === 'switch-to-sign-in'){
+    signUpWrapper.style.display = 'none';
+    signInWrapper.style.display = 'block';
+  }else{
+    signInWrapper.style.display = 'none';
+    signUpWrapper.style.display = 'block';
+  }
+};
 function modalDisplay({target}){
     target.id === "sign-in-btn" ? signInWrapper.style.display = "block" : signUpWrapper.style.display = "block" ;
     document.body.addEventListener('keydown', closeModal);
