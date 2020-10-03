47ad367441201d4d078739a8c9dee526ad47b40d
Joel Delatte <joeldelatte@gmail.com>
2020-10-03 10:15:32 -0500

    update .env and remove cached files

--- components/Footer/Footer.component.jsx
+++ components/Footer/Footer.component.jsx
@@ -1,6 +1,6 @@
 import Axios from "axios";
 import styles from "./Footer.module.scss";
-import axios from "axios";
+
 
 class Footer extends React.Component {
 	constructor(props) {
@@ -13,15 +13,15 @@ class Footer extends React.Component {
 		this.handleSubscribe = this.handleSubscribe.bind(this);
 	}
 
-	async handleSubscribe() {
-		// this.setState({fName: firstName, lName: lastName, email: emailValue})
-
+	async handleSubscribe(e) {
+		e.preventDefault();
 		try {
 			const response = await Axios.post("api/newsletter", { email, fName, lName})
 			console.log("it worked, bro, you're signed up to the newsletter")
-		} catch (e) {
-			console.log(e.response.data.error)
+		} catch {
+			console.log("there was an error")
 		}
+		document.querySelector(".newsForm").reset()
 	}
 
 	render() {
@@ -37,12 +37,12 @@ class Footer extends React.Component {
 						<p>info@joiningallmusicians.com</p>
 					</div>
 					<div className={styles.subscribeContainer}>
-						<form>
+						<form className="newsForm">
 							<h2>STAY TUNED</h2>
 							<input type='text' placeholder='First name' onChange={(e) => this.setState({fName: e.target.value})}></input>
 							<input type='text' placeholder='Last name' onChange={(e) => this.setState({lName: e.target.value})}></input>
 							<input type='email' placeholder='Email' onChange={(e) => this.setState({email: e.target.value})}></input>
-							<button type='submit' onClick={this.handleSubscribe}>Subscribe</button>
+							<button type='submit' onClick={(e)=>this.handleSubscribe(e)}>Subscribe</button>
 						</form>
 					</div>
 				</footer>
