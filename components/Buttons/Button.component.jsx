26ddbf497b75772d6e7556ded485e9416905cd9d
Karson Krieg <klkrieg3@gmail.com>
2020-10-07 22:48:37 -0700

    Financial filter and status buttons are correctly updating their states

--- components/Buttons/Button.component.jsx
+++ components/Buttons/Button.component.jsx
@@ -1,8 +1,23 @@
 import styles from "./Button.module.scss";
 const Button = (props) => {
 	const handleClick = (e) => {
-		props.filterClick();
+		props.filterClick(props.id);
+
+		// if (status == ["all"] && target == "all") {
+		// 	return
+		// }else if(status == ["all"] && target != "all"){
+
+		// }
+		//if status == ['all'] and the button pressed == all there should
+		//be no change in either status or the color of the all button
+
+		//if status == ['all'] and the button pressed != all the
+		//color of all should change as well as the color of the button pressed && status == ["button pressed"]
+
+		//if status != ['all'] && status includes the button pressed the button pressed should change
+
 		const target = e.target;
+
 		target.className == styles.unClicked
 			? (target.className = styles.clicked)
 			: (target.className = styles.unClicked);
@@ -10,7 +25,11 @@ const Button = (props) => {
 
 	return (
 		<div className={styles.regButton}>
-			<button className={styles.clicked} id={props.id} onClick={handleClick}>
+			<button
+				className={props.id == "all" ? styles.clicked : styles.unClicked}
+				id={props.id}
+				onClick={handleClick}
+			>
 				{props.children}
 			</button>
 		</div>
--- pages/resources.jsx
+++ pages/resources.jsx
@@ -4,15 +4,16 @@ import Button from "../components/Buttons/Button.component";
 import styles from "../pageStyles/resources.module.scss";
 import { useEffect, useState } from "react";
 import dbConnect from "../utils/dbConnect";
-import Resource from "../models/resource";
+//import Resource from "../models/resource";
+import resourceOffline from "../utils/resources-for-offline";
 
 const Resources = ({ resourceData }) => {
 	//Creating a state for the different filtering and sorting controllers
-	// const [financialGroup, setFinancialGroup] = useState({
-	// 	pandemicImpact: false,
-	// 	industryProfessional: false,
-	// 	incomeMin: false,
-	// });
+	const [financialGroup, setFinancialGroup] = useState({
+		pandemicImpact: false,
+		industryProfessional: false,
+		incomeMin: false,
+	});
 
 	// const [roleGroup, setRoleGroup] = useState({
 	// 	musician: false,
@@ -24,7 +25,7 @@ const Resources = ({ resourceData }) => {
 	// const [years, setYears] = useState("1");
 
 	//usestate hook for filter buttons
-	const [filters, setFilters] = useState([]);
+	const [status, setStatus] = useState(["all"]);
 	//Use effect to listen to filter buttons and update state conditionally
 
 	//This is what we will refer to to load user inputs
@@ -32,10 +33,38 @@ const Resources = ({ resourceData }) => {
 	// 	...financialGroup,
 	// 	...roleGroup,
 	// 	years,
-	// 	filters,
+	// 	status,
 	// 	sort,
 	// };
 
+	const handleFinancialFilterButton = (id) => {
+		setFinancialGroup((financialGroup) => ({ ...financialGroup, [id]: !financialGroup[id] }));
+	};
+	//assigns status based on the id of the targets
+	const handleStatusFilterButton = (id) => {
+		if (id == "all") {
+			setStatus(["all"]);
+		} else if (id != "all" && status.includes("all")) {
+			setStatus([id]);
+		} else if (status == [id]) {
+			setStatus(["all"]);
+		} else if (id != "all" && status.includes(id)) {
+			if (id != "all" && status.length == 1 && status.indexOf("all") == -1) {
+				setStatus(["all"]);
+			} else {
+				const newStatus = status.filter((option) => option != id);
+				setStatus(newStatus);
+			}
+		} else {
+			setStatus((status) => [...status, id]);
+		}
+	};
+	//Effect for financial Group change
+	//This will end updating resource results based on change in financialgroup state
+	useEffect(() => {
+		console.log(financialGroup);
+		console.log(status);
+	}, [financialGroup, status]);
 	const [resource, setResource] = useState(resourceData);
 	//handles the removal of irrelavent items
 	const handleNotRelevantClick = (key) => {
@@ -44,99 +73,101 @@ const Resources = ({ resourceData }) => {
 				return resource._id !== key;
 			})
 		);
+		console.log(resource);
 	};
 	return (
 		<Layout>
-			<div>
-				<div className={styles.controllers}>
-					<h1>Are you a musician that has been affected by the COVID-19 Pandemic?</h1>
-					<p className={styles.subtitle}>
-						We’ve built a clear and valuable toolkit of Support Resources available to
-						Austin musicians and industry.
-					</p>
-					<div className={styles.financialGroup}>
-						<h2>Select what you can provide and/or is true for you…</h2>
-						<Button id='pandemicImpact'>
-							Proof that you were financially impacted by the pandemic. (e.g. lost
-							gigs, shows, bookings, contracts, unemployment etc.)
+			<div className={styles.controllers}>
+				<h1>Are you a musician that has been affected by the COVID-19 Pandemic?</h1>
+				<p className={styles.subtitle}>
+					We’ve built a clear and valuable toolkit of Support Resources available to
+					Austin musicians and industry.
+				</p>
+				<div className={styles.financialGroup}>
+					<h2>Select what you can provide and/or is true for you…</h2>
+
+					<Button filterClick={handleFinancialFilterButton} id='pandemicImpact'>
+						Proof that you were financially impacted by the pandemic. (e.g. lost gigs,
+						shows, bookings, contracts, unemployment etc.)
+					</Button>
+					<Button filterClick={handleFinancialFilterButton} id='industryProfessional'>
+						Proof of professional musicianship and/or music industry employment.
+					</Button>
+					<Button filterClick={handleFinancialFilterButton} id='incomeMin'>
+						The music industry is over 50% of my income
+					</Button>
+				</div>
+				<div className={styles.divider}></div>
+				<div className={styles.rolesGroup}>
+					<a>
+						<p id='musician'>Musician</p>
+					</a>
+					<div className={styles.vertDivider}></div>
+					<a>
+						<p id='engineer'>Engineer/Crew</p>
+					</a>
+					<div className={styles.vertDivider}></div>
+					<a>
+						<p id='business'>Business</p>
+					</a>
+					<div className={styles.vertDivider}></div>
+					<a>
+						<p id='educator'>Educator</p>
+					</a>
+				</div>
+				<label>
+					<span>Years in industry:</span> 1
+					<input type='range' min='1' max='20' step='1'></input> 20+
+				</label>
+				<div className={styles.divider}></div>
+				<div className={styles.filtersAndSortContainer}>
+					<div className={styles.filterContainer}>
+						<p>Would you like to see?</p>
+						<Button filterClick={handleStatusFilterButton} status={status} id='all'>
+							All
 						</Button>
-						<Button id='industryProfessional'>
-							{" "}
-							Proof of professional musicianship and/or music industry employment.
+						<Button filterClick={handleStatusFilterButton} status={status} id='open'>
+							Open
 						</Button>
-						<Button id='incomeMin'>The music industry is over 50% of my income</Button>
+						<Button filterClick={handleStatusFilterButton} status={status} id='wait'>
+							Waitlisted
+						</Button>
+						<Button filterClick={handleStatusFilterButton} status={status} id='closed'>
+							Closed
+						</Button>
 					</div>
-					<div className={styles.divider}></div>
-					<div className={styles.rolesGroup}>
-						<a>
-							<p id='musician'>Musician</p>
-						</a>
-						<div className={styles.vertDivider}></div>
-						<a>
-							<p id='engineer'>Engineer/Crew</p>
-						</a>
-						<div className={styles.vertDivider}></div>
-						<a>
-							<p id='business'>Business</p>
-						</a>
-						<div className={styles.vertDivider}></div>
-						<a>
-							<p id='educator'>Educator</p>
-						</a>
-					</div>
-					<label>
-						<span>Years in industry:</span> 1
-						<input type='range' min='1' max='20' step='1'></input> 20+
-					</label>
-					<div className={styles.divider}></div>
-					<div className={styles.filtersAndSortContainer}>
-						<div className={styles.filterContainer}>
-							<p>Would you like to see?</p>
-							<Button filterClick={() => handleFilterClick("all")} id='all'>
-								All
-							</Button>
-							<Button filterClick={() => handleFilterClick("open")} id='open'>
-								Open
-							</Button>
-							<Button filterClick={() => handleFilterClick("wait")} id='wait'>
-								Waitlisted
-							</Button>
-							<Button filterClick={() => handleFilterClick("closed")} id='closed'>
-								Closed
-							</Button>
-						</div>
-						<div className={styles.sortContainer}>
-							<span>SORT:</span>
-							<p>A-Z</p>
-							<div className={styles.vertDividerThin}></div>
-							<p>Z-A</p>
-							<div className={styles.vertDividerThin}></div>
-							<p>$</p>
-						</div>
+					<div className={styles.sortContainer}>
+						<span>SORT:</span>
+						<p>A-Z</p>
+						<div className={styles.vertDividerThin}></div>
+						<p>Z-A</p>
+						<div className={styles.vertDividerThin}></div>
+						<p>$</p>
 					</div>
 				</div>
-				<div className={styles.resultsContainer}>
-					{resource.map((data) => {
-						return (
-							<ResourceCard
-								key={data["_id"]}
-								name={data.organization}
-								//benifit={resource.benefit}
-								status={data.status}
-								handleNotRelevantClick={() => handleNotRelevantClick(data["_id"])}
-								//eligibilities={resource.eligibilities}
-							/>
-						);
-					})}
-				</div>
+			</div>
+			<div className={styles.resultsContainer}>
+				{resource.map((data) => {
+					return (
+						<ResourceCard
+							key={data["_id"]}
+							name={data.organization}
+							//benifit={resource.benefit}
+							status={data.status}
+							handleNotRelevantClick={() => handleNotRelevantClick(data["_id"])}
+							//eligibilities={resource.eligibilities}
+						/>
+					);
+				})}
 			</div>
 		</Layout>
 	);
 };
 
 export async function getStaticProps() {
-	dbConnect();
-	const data = await Resource.find({}, "organization _id status");
+	//dbConnect();
+	//const data = await Resource.find({}, "organization _id status");
+	const data = resourceOffline;
 	const string = JSON.stringify(data);
 	const resourceData = JSON.parse(string);
 	return {
