b31c2db02b2d8dcc72bb973594ed2de9a5d87e51
Matt Bise <mbise1993@gmail.com>
2020-11-05 23:38:40 -0600

    Convert Footer to functional component

--- components/Footer/Footer.component.jsx
+++ components/Footer/Footer.component.jsx
@@ -3,71 +3,71 @@ import React from 'react';
 
 import styles from './Footer.module.scss';
 
-class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fName: '',
-      lName: '',
-      email: '',
-    };
-    this.handleSubscribe = this.handleSubscribe.bind(this);
-  }
+const Footer = () => {
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+  const [email, setEmail] = React.useState('');
 
-  async handleSubscribe(e) {
+  const handleSubscribeClick = async (e) => {
     e.preventDefault();
+
     try {
+      // TODO: Sign up and handle response
+      // eslint-disable-next-line no-unused-vars
       const response = await Axios.post('api/newsletter', {
         email,
-        fName,
-        lName,
+        firstName,
+        lastName,
       });
-      console.log("it worked, bro, you're signed up to the newsletter");
-    } catch (e) {
-      console.log('there was an error');
-    }
-    document.querySelector('.newsForm').reset();
-  }
 
-  render() {
-    return (
-      <div>
-        <footer className={styles.footerContainer}>
-          <div className={styles.logoContainer}>
-            <img alt="JAM logo" src="assets/jam-logo-1.png"></img>
-            <p>Austin, Texas</p>
-          </div>
-          <div className={styles.contactContainer}>
-            <h2>CONTACT INFO</h2>
-            <p>info@joiningallmusicians.com</p>
-          </div>
-          <div className={styles.subscribeContainer}>
-            <form className="newsForm">
-              <h2>STAY TUNED</h2>
-              <input
-                type="text"
-                placeholder="First name"
-                onChange={(e) => this.setState({ fName: e.target.value })}
-              ></input>
-              <input
-                type="text"
-                placeholder="Last name"
-                onChange={(e) => this.setState({ lName: e.target.value })}
-              ></input>
-              <input
-                type="email"
-                placeholder="Email"
-                onChange={(e) => this.setState({ email: e.target.value })}
-              ></input>
-              <button type="submit" onClick={(e) => this.handleSubscribe(e)}>
-                Subscribe
-              </button>
-            </form>
-          </div>
-        </footer>
-      </div>
-    );
-  }
-}
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+    } catch (e) {
+      console.log(`Signup error: ${e}`);
+    }
+  };
+
+  return (
+    <div>
+      <footer className={styles.footerContainer}>
+        <div className={styles.logoContainer}>
+          <img alt="JAM logo" src="assets/jam-logo-1.png"></img>
+          <p>Austin, Texas</p>
+        </div>
+        <div className={styles.contactContainer}>
+          <h2>CONTACT INFO</h2>
+          <p>info@joiningallmusicians.com</p>
+        </div>
+        <div className={styles.subscribeContainer}>
+          <form className="newsForm">
+            <h2>STAY TUNED</h2>
+            <input
+              type="text"
+              placeholder="First name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            ></input>
+            <input
+              type="text"
+              placeholder="Last name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            ></input>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <button type="submit" onClick={handleSubscribeClick}>
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
