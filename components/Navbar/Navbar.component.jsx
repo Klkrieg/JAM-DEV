13e217ee238a1d78027eb57059086f26f38f85bd
Matt Bise <mbise1993@gmail.com>
2020-11-06 00:04:26 -0600

    Convert Navbar to functional component

--- components/Navbar/Navbar.component.jsx
+++ components/Navbar/Navbar.component.jsx
@@ -6,97 +6,76 @@ import SignInModal from '../Modals/SignInModal.component';
 import SignUpModal from '../Modals/SignUpModal.component';
 import styles from './Navbar.module.scss';
 
-class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      signInOpen: false,
-      signUpOpen: false,
+const Navbar = () => {
+  const [isSignInOpen, setSignInOpen] = React.useState(false);
+  const [isSignUpOpen, setSignUpOpen] = React.useState(false);
+
+  React.useLayoutEffect(() => {
+    const handleOffModalClick = (e) => {
+      if (e.target.className.match(/backdrop/i)) {
+        setSignInOpen(false);
+        setSignUpOpen(false);
+      }
     };
-    this.handleEscKeyPress = this.handleEscKeyPress.bind(this);
-    this.handleOffModalClick = this.handleOffModalClick.bind(this);
-    this.handleModalSwitch = this.handleModalSwitch.bind(this);
-  }
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleEscKeyPress);
-    document.addEventListener('click', this.handleOffModalClick);
-  }
 
-  handleSignInOpen() {
-    this.setState({ signInOpen: true });
-  }
-  handleSignUpOpen() {
-    this.setState({ signUpOpen: true });
-  }
+    const handleEscKeyPress = (e) => {
+      if (e.keyCode == 27) {
+        setSignInOpen(false);
+        setSignUpOpen(false);
+      }
+    };
 
-  handleOffModalClick(e) {
-    if (e.target.className.match(/backdrop/i)) {
-      this.setState({
-        signInOpen: false,
-        signUpOpen: false,
-      });
-    }
-  }
-  handleEscKeyPress(e) {
-    if (e.keyCode == 27) {
-      this.setState({
-        signInOpen: false,
-        signUpOpen: false,
-      });
-    }
-  }
-  handleModalSwitch(e) {
-    let text = e.target.innerHTML;
-    text == 'Sign up.'
-      ? this.setState({
-          signInOpen: false,
-          signUpOpen: true,
-        })
-      : this.setState({
-          signInOpen: true,
-          signUpOpen: false,
-        });
-    console.log(e.target.innerHTML);
-  }
+    document.addEventListener('click', handleOffModalClick);
+    document.addEventListener('keydown', handleEscKeyPress);
 
-  render() {
-    return (
-      <div className={styles.Navbar}>
-        <img
-          alt="JAM logo"
-          src="/assets/jam-logo-1.png"
-          className={styles.navbar__logo}
-        />
-        <ul className={styles.PageLinks}>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/connect">Connect</Link>
-          </li>
-          <li>
-            <Link href="/resources">Resources</Link>
-          </li>
-        </ul>
-        <div className={styles.buttonContainer}>
-          <button className={styles.orange} onClick={this.handleSignInOpen}>
-            Sign in
-          </button>
-          <button className={styles.white} onClick={this.handleSignUpOpen}>
-            Sign up
-          </button>
-        </div>
-        {this.state.signInOpen && <Backdrop />}
-        {this.state.signInOpen && (
-          <SignInModal switchHandle={this.handleModalSwitch} />
-        )}
-        {this.state.signUpOpen && <Backdrop />}
-        {this.state.signUpOpen && (
-          <SignUpModal switchHandle={this.handleModalSwitch} />
-        )}
+    return () => {
+      document.removeEventListener('click', handleOffModalClick);
+      document.removeEventListener('keydown', handleEscKeyPress);
+    };
+  }, []);
+
+  const handleSwitchToSignUpClick = () => {
+    setSignInOpen(false);
+    setSignUpOpen(true);
+  };
+
+  const handleSwitchToSignInClick = () => {
+    setSignUpOpen(false);
+    setSignInOpen(true);
+  };
+
+  return (
+    <div className={styles.Navbar}>
+      <img
+        alt="JAM logo"
+        src="/assets/jam-logo-1.png"
+        className={styles.navbar__logo}
+      />
+      <ul className={styles.PageLinks}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/connect">Connect</Link>
+        </li>
+        <li>
+          <Link href="/resources">Resources</Link>
+        </li>
+      </ul>
+      <div className={styles.buttonContainer}>
+        <button className={styles.orange} onClick={() => setSignInOpen(true)}>
+          Sign in
+        </button>
+        <button className={styles.white} onClick={() => setSignUpOpen(true)}>
+          Sign up
+        </button>
       </div>
-    );
-  }
-}
+
+      {(isSignInOpen || isSignUpOpen) && <Backdrop />}
+      {isSignInOpen && <SignInModal switchHandle={handleSwitchToSignUpClick} />}
+      {isSignUpOpen && <SignUpModal switchHandle={handleSwitchToSignInClick} />}
+    </div>
+  );
+};
 
 export default Navbar;
