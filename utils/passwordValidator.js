6a9a98c38b2ee27e6f68c90342b2d9c475c1ba3f
Matt Bise <mbise1993@gmail.com>
2020-11-05 20:06:23 -0600

    Fix lint errors in /utils

--- utils/passwordValidator.js
+++ utils/passwordValidator.js
@@ -1,26 +1,27 @@
-import * as passwordValidator from "password-validator";
+import * as passwordValidator from 'password-validator';
+
 export default function validator(password) {
-	//password field validator
-	///////////
+  //password field validator
+  ///////////
 
-	let passwordSchema = new passwordValidator();
+  let passwordSchema = new passwordValidator();
 
-	passwordSchema
-		.is()
-		.min(8) // Minimum length 8
-		.is()
-		.max(50) // Maximum length 50
-		.has()
-		.uppercase(1) // Must have uppercase letters
-		.has()
-		.lowercase(1) // Must have lowercase letters
-		.has()
-		.digits(1) // Must have at least 1 digits
-		.has()
-		.symbols(1) // Must have at least 1 symbol
-		.has()
-		.not()
-		.spaces();
+  passwordSchema
+    .is()
+    .min(8) // Minimum length 8
+    .is()
+    .max(50) // Maximum length 50
+    .has()
+    .uppercase(1) // Must have uppercase letters
+    .has()
+    .lowercase(1) // Must have lowercase letters
+    .has()
+    .digits(1) // Must have at least 1 digits
+    .has()
+    .symbols(1) // Must have at least 1 symbol
+    .has()
+    .not()
+    .spaces();
 
-	return passwordSchema.validate(password, { list: true });
+  return passwordSchema.validate(password, { list: true });
 }
