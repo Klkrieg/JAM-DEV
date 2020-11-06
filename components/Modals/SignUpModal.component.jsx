aec90d16ca622237e069aeb06710d171c040875f
Matt Bise <mbise1993@gmail.com>
2020-11-06 00:04:01 -0600

    Convert Modals to functional components

--- components/Modals/SignUpModal.component.jsx
+++ components/Modals/SignUpModal.component.jsx
@@ -3,33 +3,30 @@ import React from 'react';
 import styles from './SignUpModal.module.scss';
 import validator from '../../utils/passwordValidator';
 
-class SignUpModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      zipcode: '',
-      password: '',
-      profileType: '',
-      errors: {
-        email: '',
-        zipcode: '',
-        password: [],
-      },
-    };
-  }
+const SignUpModal = ({ switchHandle }) => {
+  const [form, setForm] = React.useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    zipCode: '',
+    password: '',
+    profileType: '',
+  });
 
-  handleSubmit(event) {
-    event.preventDefault();
-  }
+  const [errors, setErrors] = React.useState({
+    email: '',
+    zipCode: '',
+    password: [],
+  });
 
-  handlePasswordChange(pswd) {
-    let errs = validator(pswd);
-    console.log(errs);
-    if (errs.length > 0) {
-      let errors = errs.map((error) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handlePasswordChange = (password) => {
+    let passwordErrors = validator(password);
+    if (passwordErrors.length > 0) {
+      const errorMessages = passwordErrors.map((error) => {
         switch (error) {
           case 'min':
             return 'eight characters';
@@ -47,155 +44,146 @@ class SignUpModal extends React.Component {
             return 'no spaces';
         }
       });
-      this.setState(
-        {
-          errors: {
-            password: errors,
-          },
-        },
-        //() => console.log(this.state.errors)
-      );
-    } else if (!errs.length) {
-      this.setState({
-        errors: {
-          password: [],
-        },
-      });
-      console.log('Pass pass');
-    }
-  }
 
-  handleChange(e) {
+      setErrors({
+        ...errors,
+        password: errorMessages,
+      });
+    } else {
+      setErrors({
+        ...errors,
+        password: [],
+      });
+    }
+  };
+
+  const handleChange = (e) => {
     let id = e.target.id;
     let value = e.target.value;
     if (id == 'password') {
-      this.handlePasswordChange(value);
+      handlePasswordChange(value);
     }
-    this.setState({
+
+    setForm({
+      ...form,
       [id]: value,
     });
-  }
-  handleButtonClick(e) {
-    this.setState(
-      {
-        profileType: e.target.name,
-      },
-      () => console.log(this.state),
-    );
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <div className={styles.modal}>
-          <h1>Sign up</h1>
-          <form className={styles.SignUpModal} onSubmit={this.handleSubmit}>
-            <div className={styles.fName}>
-              <label htmlFor="firstName">First name</label>
-              <input
-                type="text"
-                id="firstName"
-                value={this.state.firstName}
-                onChange={this.handleChange}
-                required
-              ></input>
-            </div>
-            <div className={styles.lName}>
-              <label htmlFor="lastName">Last name</label>
-              <input
-                type="text"
-                id="lastName"
-                value={this.state.lastName}
-                onChange={this.handleChange}
-                required
-              ></input>
-            </div>
-            <div className={styles.email}>
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                id="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-                required
-              ></input>
-            </div>
-            <div className={styles.zip}>
-              <label htmlFor="zipcode">Zip code</label>
-              <input
-                type="text"
-                id="zipcode"
-                value={this.state.zipcode}
-                onChange={this.handleChange}
-                required
-              ></input>
-            </div>
-            <div className={styles.password}>
-              <label htmlFor="password">Password</label>
+  const handleButtonClick = (e) => {
+    setForm({
+      ...form,
+      profileType: e.target.name,
+    });
+  };
 
-              {!this.state.errors.password.length ? (
-                <span></span>
-              ) : (
-                <span>{`Your password must contain: ${this.state.errors.password.join(
-                  ', ',
-                )}`}</span>
-              )}
-              <input
-                type="password"
-                id="password"
-                value={this.state.password}
-                onChange={this.handleChange}
-                required
-              ></input>
+  return (
+    <div>
+      <div className={styles.modal}>
+        <h1>Sign up</h1>
+        <form className={styles.SignUpModal} onSubmit={handleSubmit}>
+          <div className={styles.fName}>
+            <label htmlFor="firstName">First name</label>
+            <input
+              type="text"
+              id="firstName"
+              value={form.firstName}
+              onChange={handleChange}
+              required
+            ></input>
+          </div>
+          <div className={styles.lName}>
+            <label htmlFor="lastName">Last name</label>
+            <input
+              type="text"
+              id="lastName"
+              value={form.lastName}
+              onChange={handleChange}
+              required
+            ></input>
+          </div>
+          <div className={styles.email}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              value={form.email}
+              onChange={handleChange}
+              required
+            ></input>
+          </div>
+          <div className={styles.zip}>
+            <label htmlFor="zipcode">Zip code</label>
+            <input
+              type="text"
+              id="zipCode"
+              value={form.zipcode}
+              onChange={handleChange}
+              required
+            ></input>
+          </div>
+          <div className={styles.password}>
+            <label htmlFor="password">Password</label>
+            {errors.password.length > 0 && (
+              <span>{`Your password must contain: ${this.state.errors.password.join(
+                ', ',
+              )}`}</span>
+            )}
+            <input
+              type="password"
+              id="password"
+              value={form.password}
+              onChange={handleChange}
+              required
+            ></input>
+          </div>
+          <div className={styles.confirmPassword}>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input type="password" id="confirmPassword" required></input>
+          </div>
+          <div className={styles.profileType}>
+            <label htmlFor="profileTypeContainer">Profile Type</label>
+            <div className={styles.buttonContainer} id="profileTypeContainer">
+              <button
+                type="button"
+                name="Ind_prof"
+                onClick={handleButtonClick}
+                className={styles.profileBtn}
+              >
+                Indivudual/Professional
+              </button>
+              <button
+                type="button"
+                name="Band"
+                onClick={handleButtonClick}
+                className={styles.profileBtn}
+              >
+                Band
+              </button>
+              <button
+                type="button"
+                name="Org_biz"
+                onClick={handleButtonClick}
+                className={styles.profileBtn}
+              >
+                Organization/Business
+              </button>
             </div>
-            <div className={styles.confirmPassword}>
-              <label htmlFor="confirmPassword">Confirm Password</label>
-              <input type="password" id="confirmPassword" required></input>
-            </div>
-            <div className={styles.profileType}>
-              <label htmlFor="profileTypeContainer">Profile Type</label>
-              <div className={styles.buttonContainer} id="profileTypeContainer">
-                <button
-                  type="button"
-                  name="Ind_prof"
-                  onClick={this.handleButtonClick}
-                  className={styles.profileBtn}
-                >
-                  Indivudual/Professional
-                </button>
-                <button
-                  type="button"
-                  name="Band"
-                  onClick={this.handleButtonClick}
-                  className={styles.profileBtn}
-                >
-                  Band
-                </button>
-                <button
-                  type="button"
-                  name="Org_biz"
-                  onClick={this.handleButtonClick}
-                  className={styles.profileBtn}
-                >
-                  Organization/Business
-                </button>
-              </div>
-            </div>
+          </div>
 
-            <div className={styles.switch}>
-              <p>Already have an account?</p>
-              <a className={styles.bold} onClick={this.props.switchHandle}>
-                Sign in.
-              </a>
-            </div>
-            <button type="submit" className={styles.submit}>
-              Sign in
-            </button>
-          </form>
-        </div>
+          <div className={styles.switch}>
+            <p>Already have an account?</p>
+            <a className={styles.bold} onClick={switchHandle}>
+              Sign in.
+            </a>
+          </div>
+          <button type="submit" className={styles.submit}>
+            Sign in
+          </button>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SignUpModal;
